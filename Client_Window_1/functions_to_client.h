8fb5d4d88e13b099e672418df19cd76d70dd2f08
Марк Лендер 241-3211 <lendermark114@gmail.com>
2025-04-29 21:47:33 +0300

    Просто почистил ветку

--- Client_Window_1/functions_to_client.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTIONS_TO_CLIENT_H
-#define FUNCTIONS_TO_CLIENT_H
-
-#include <QString>
-
-bool auth(QString, QString);
-bool reg(QString, QString, QString);
-#endif // FUNCTIONS_TO_CLIENT_H
--- ConsoleClient/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <QCoreApplication>
-#include "clientapi.h"
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    qDebug()<<ClientAPI::getInstance()->query_to_server("AAAAAAAAAAA");
-    return a.exec();
-}
