b84f1d6a1eeea2374131e2e62912bedcb538ac0b
Daniil Netkachev <daniilnetkacev742@gmail.com>
2025-05-23 10:04:49 +0300

    Delete Client_Window_2 directory

--- Client_Window_2/functions_to_client.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_TO_CLIENT_H
-#define FUNCTIONS_TO_CLIENT_H
-
-#include <QString>
-
-bool auth(const QString& login, const QString& password);
-bool reg(const QString& login, const QString& password, const QString& mail);
-
-#endif // FUNCTIONS_TO_CLIENT_H
